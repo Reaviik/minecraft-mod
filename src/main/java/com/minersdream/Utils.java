efc672918705a92347eb4c9678da1a27cc852630
Player_rs <josueplayerrs@gmail.com>
2023-01-25 00:25:43 -0300

    * Utilidades, inuteis

---
+++ src/main/java/com/minersdream/Utils.java
@@ -0,0 +1,22 @@
+package com.minersdream;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Utils extends Item {
+
+    public Utils(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        // COLAR AQUI O QUE QUISER, SE QUISER :D
+    }
+}

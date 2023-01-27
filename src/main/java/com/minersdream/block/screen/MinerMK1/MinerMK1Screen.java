6e74aca6ecc7042b3ecb49751e6e6c3cab2b09b1
David Herbert Schultz <reavik_gamer@hotmail.com>
2023-01-27 14:47:55 -0300

    * new bugs and more bugs

--- src/main/java/com/minersdream/block/screen/MinerMK1/MinerMK1Screen.java
+++ src/main/java/com/minersdream/block/screen/MinerMK1/MinerMK1Screen.java
@@ -22,14 +22,14 @@ public class MinerMK1Screen extends AbstractContainerScreen<MinerMK1Menu> {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
-        this.imageWidth = 256-80;
-        this.imageHeight = 256+18;
+        this.imageWidth = 256;
+        this.imageHeight = 256;
         int x = (width - imageWidth) / 2;
         int y = (height - imageHeight) / 2;
 
 
 
-        this.blit(pPoseStack, x, y, 0, 0, imageWidth, imageHeight);
+        this.blit(pPoseStack, x+40, y-9, 0, 0, imageWidth, imageHeight);
 
 //        if(menu.isCrafting()) {
 //            blit(pPoseStack, x + 102, y + 41, 176, 0, 8, menu.getScaledProgress());
